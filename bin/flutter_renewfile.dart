571ac59f1d5a1d70241b38aa0939f4667272e318
dxc_123 <daixingchuang@163.com>
2025-02-06 14:07:17 +0800

    feat: 实时显示进度，格式为 [当前进度/总数]

--- bin/flutter_renewfile.dart
+++ bin/flutter_renewfile.dart
@@ -61,39 +61,53 @@ void main(List<String> arguments) {
     }
 
     if (results.rest.contains('renew')) {
-      final directory = Directory.current; // 获取当前目录
-      logInfo('Scanning directory: ${directory.path}');
-      processDirectory(directory);
+      processDirectory();
     }
   } on FormatException catch (e) {
     // Print usage information if an invalid argument was provided.
-    logError("error = ${e.message}");
+    logError("❌error = ${e.message}");
     printUsage(argParser);
   }
 }
 
-void processDirectory(Directory directory) {
-  for (var entity in directory.listSync(recursive: true)) {
-    if (entity is File && entity.path.endsWith('.dart')) {
-      copyAndReplaceDartFile(entity);
-    }
+void processDirectory() {
+  final directory = Directory.current; // 获取当前目录
+  logInfo('Scanning directory: ${directory.path}');
+
+  final files = getDartFiles(directory);
+  final totalFiles = files.length;
+
+  if (totalFiles == 0) {
+    logError('❌No .dart files found.');
+    return;
   }
+
+  logInfo('Total .dart files: $totalFiles');
+
+  for (var i = 0; i < files.length; i++) {
+    final file = files[i];
+    processDartFile(file, i + 1, totalFiles);
+  }
+
+  logSuccess('✅ All files processed successfully!');
 }
 
-void copyAndReplaceDartFile(File file) {
-  final tempFilePath = '${file.path}_copy'; // 先创建临时文件
+List<File> getDartFiles(Directory directory) {
+  return directory.listSync(recursive: true).whereType<File>().where((file) => file.path.endsWith('.dart')).toList();
+}
+
+void processDartFile(File file, int index, int total) {
+  final tempFilePath = '${file.path}_copy'; // 临时文件
   final tempFile = File(tempFilePath);
 
   try {
     final content = file.readAsStringSync();
     tempFile.writeAsStringSync(content);
-    file.deleteSync(); // 删除旧文件
+    file.deleteSync(); // 删除原文件
+    tempFile.renameSync(file.path); // 还原原始文件名
 
-    final newFilePath = file.path; // 还原原始文件名
-    tempFile.renameSync(newFilePath);
-
-    logSuccess('Replaced: ${file.path}');
+    logInfo('[$index/$total] Processed: ${file.path}');
   } catch (e) {
-    logError('Error processing file ${file.path}: $e');
+    logError('❌ Error processing file ${file.path}: $e');
   }
 }
